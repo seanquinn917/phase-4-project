c36371cddfff923c2f4226de78c9372d26190c4c
SeanQuinn <Seanquinn917@gmail.com>
2024-06-09 21:15:20 -0400

    case sensitivity for login debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :username, uniqueness: true
     
+    
     has_secure_password
 
     has_many :reviews, dependent: :destroy
